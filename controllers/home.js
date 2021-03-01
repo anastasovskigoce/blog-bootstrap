16b31682ef021d60786d244b6a1de1e217bad6ff
Goce <ganastasovski@alarm.com>
2021-03-01 10:44:05 -0500

    added mode functionality

---
+++ controllers/home.js
@@ -0,0 +1,9 @@
+const { models } = require('mongoose')
+const BlogPost = require('../models/BlogPost.js')
+
+module.exports = async (req, res) => {
+    const blogposts = await BlogPost.find({})
+    res.render('index', {
+        blogposts
+    });
+}
--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
+app.use(fileUpload())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
 
 app.listen(4000,() =>{
     console.log("App listening on port 4000")
 })
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+const validateMiddleWare = require('./middleware/validationMiddleware');
+app.use('/posts/store', validateMiddleWare)
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+const homeController = require('./controllers/home')
+app.get('/', homeController)
 
-app.get('/contact', (req, res) => {
-    res.render('contact')
-})
+const getPostController = require('./controllers/getPost')
+app.get('/post/:id', getPostController)
 
-app.get('/post', (req, res) => {
-    res.render('post')
-})
+const newPostConroller = require('./controllers/newPost')
+app.get('/posts/new', newPostConroller)
+
+const storePostController = require('./controllers/storePost')
+app.post('/posts/store', storePostController)
